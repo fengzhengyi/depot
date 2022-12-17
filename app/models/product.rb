77097fe26512d4ad6889c15534907c9c897f07c2
fengzhengyi <512369179@qq.com>
2022-12-17 15:58:36 +0800

    rails style guides:single-attribute Validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :image_url, presence: true, image: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, image: true
 
 end
 
