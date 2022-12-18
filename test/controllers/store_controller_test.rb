ae6c531491de41d5c07f1ff3b8efc0d8c13cfbb2
fengzhengyi <512369179@qq.com>
2022-12-18 08:02:28 +0800

    functional testing of controllers

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select 'main ul li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select 'div', /\$[,\d]+\.\d\d/
   end
 end
